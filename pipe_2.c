6c22839d2820552ec51351e43ff960a8dd0153a6
Praveen Chaubey <praveen.k.chaubey@gmail.com>
2022-09-01 15:48:29 +0530

    Update pipe programs

--- pipe_2.c
+++ pipe_2.c
@@ -7,40 +7,38 @@
 
 int main(void)
 {
-	int pfds[2];
-	pid_t pid;
+    int pfds[2];
+    pid_t pid;
 
-	if(-1 == pipe(pfds))
-	{
-		printf("Error in creating a pipe\n");
-		perror("Pipe Error");
-		exit(-1);
-	}
+    if(-1 == pipe(pfds))
+    {
+        printf("Error in creating a pipe\n");
+        perror("Pipe Error");
+        exit(-1);
+    }
 
-	pid = fork();
-	if(-1 == pid)
-	{
-		printf("Error in forking the process\n");
-		perror("Fork Error");
-		exit(-1);
-	}
+    pid = fork();
+    if(-1 == pid)
+    {
+        printf("Error in forking the process\n");
+        perror("Fork Error");
+        exit(-1);
+    }
+    else if (0 == pid)
+    {
+        close(1);       /* close normal stdout */
+        dup(pfds[1]);   /* make stdout same as pfds[1] */
+        close(pfds[0]); /* we don't need this. closes the read end of the pipe */
+        execlp("ls", "ls", NULL);
+    }
+    else
+    {
+        close(0);       /* close normal stdin */
+        dup(pfds[0]);   /* make stdin same as pfds[0] */
+        close(pfds[1]); /* we don't need this. closes the write end */
+        execlp("wc", "wc", "-l", NULL);
+    }
 
-	else if (0 == pid)
-	{
-		close(1);       /* close normal stdout */
-		dup(pfds[1]);   /* make stdout same as pfds[1] */
-		close(pfds[0]); /* we don't need this. closes the read end of the pipe */
-		execlp("ls", "ls", NULL);
-	}
-
-	else
-	{
-		close(0);       /* close normal stdin */
-		dup(pfds[0]);   /* make stdin same as pfds[0] */
-		close(pfds[1]); /* we don't need this. closes the write end */
-		execlp("wc", "wc", "-l", NULL);
-	}
-
-	return 0;
+    return 0;
 }
 
--- pipe_3.c
+++ pipe_3.c
@@ -7,40 +7,40 @@
 
 int main(void)
 {
-	int pfds[2];
-	pid_t pid;
+    int pfds[2];
+    pid_t pid;
 
-	if(-1 == pipe(pfds))
-	{
-		printf("Error in creating a pipe\n");
-		perror("Pipe Error");
-		exit(-1);
-	}
+    if(-1 == pipe(pfds))
+    {
+        printf("Error in creating a pipe\n");
+        perror("Pipe Error");
+        exit(-1);
+    }
 
-	pid = fork();
-	if(-1 == pid)
-	{
-		printf("Error in forking the process\n");
-		perror("Fork Error");
-		exit(-1);
-	}
+    pid = fork();
+    if(-1 == pid)
+    {
+        printf("Error in forking the process\n");
+        perror("Fork Error");
+        exit(-1);
+    }
 
-	else if (0 == pid)
-	{
-		close(1);       /* close normal stdout */
-		dup(pfds[1]);   /* make stdout same as pfds[1] */
-		close(pfds[0]); /* we don't need this. closes the read end of the pipe */
-		execlp("ls", "ls", "-al", NULL);
-	}
+    else if (0 == pid)
+    {
+        close(1);       /* close normal stdout */
+        dup(pfds[1]);   /* make stdout same as pfds[1] */
+        close(pfds[0]); /* we don't need this. closes the read end of the pipe */
+        execlp("ls", "ls", "-al", NULL);
+    }
 
-	else
-	{
-		close(0);       /* close normal stdin */
-		dup(pfds[0]);   /* make stdin same as pfds[0] */
-		close(pfds[1]); /* we don't need this. closes the write end */
-		execlp("sort", "sort", "-n", NULL);
-	}
+    else
+    {
+        close(0);       /* close normal stdin */
+        dup(pfds[0]);   /* make stdin same as pfds[0] */
+        close(pfds[1]); /* we don't need this. closes the write end */
+        execlp("sort", "sort", "-n", NULL);
+    }
 
-	return 0;
+    return 0;
 }
 
